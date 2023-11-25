51ad865499d5719dff620b502285838b34084149
Ahmed Ikram <ahmedikram2003@gmail.com>
2023-11-25 18:47:33 +0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const data=require("file_search_simple");
-const prompt=require("prompt-sync")({sigint: true});
+const data=require("file_search_simple"); //Importing the exported module
+const prompt=require("prompt-sync")({sigint: true}); //Fot taking user input
 
 //You Can take input from user, it depends upon the way data is stored in file being searched
 let input=prompt('Enter data to search, or press [CTRL+C] to exit: ');
