ef110bf72b12ece30f1a3aadc9a3aff65af2598a
Ahmed Ikram <ahmedikram2003@gmail.com>
2023-11-25 18:46:39 +0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const data=require("file_search_simple");
 const prompt=require("prompt-sync")({sigint: true});
 
+//You Can take input from user, it depends upon the way data is stored in file being searched
 let input=prompt('Enter data to search, or press [CTRL+C] to exit: ');
 data.read('./test.txt',input);
